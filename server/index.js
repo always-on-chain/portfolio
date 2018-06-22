1c2c934665cd95658d2d7896065504daec9d2acc
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-06-21 18:27:58 -0700

    added Dotenv to server code

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,6 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-})
+app.listen(process.env.PORT || port, () => {
+  console.log(`listening on port ${process.env.PORT || port}`);
+});
